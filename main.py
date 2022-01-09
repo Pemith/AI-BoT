543d4529d9549278fefc03f8129040181ed26ee3
Pemith <pemithr@gmail.com>
2022-01-09 20:21:52 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+from newspaper import Article
+from googlesearch import search
+import random
+import nltk
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import warnings
+
+warnings.filterwarnings('ignore')
